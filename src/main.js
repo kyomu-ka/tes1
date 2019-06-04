9ef7523558120a5fc29e34103d8315827961bb02
Jodaiichi Moriuchi <jodaiich@notredame.ac.jp>
2019-06-04 18:44:09 +0900

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// BottstrapVueの読み込み
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
 
+// BootstrapVueの使用
+Vue.use(BootstrapVue)
+
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
